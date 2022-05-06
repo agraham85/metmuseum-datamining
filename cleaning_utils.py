3c64e65d2964d8195e18b06dc1285a25fd62fdc4
Michael Schmidlin <mschmidlin1@.com>
2022-05-05 22:08:35 -0400

    data cleaning done for now

--- cleaning_utils.py
+++ cleaning_utils.py
@@ -16,7 +16,17 @@ def split_rows(df, delim = "|"):
         o_nums = [o_num for i in range(len(parts))]
         newdf['Object Number'].extend(o_nums)
 
+    for col in cols:
+        print(col, len(newdf[col]))
 
-    return pd.DataFrame(newdf)
+
+    #return pd.DataFrame(newdf)
 
 
+
+
+def clean_mikes_data(df):
+    df["Dynasty_clean"] = df["Dynasty"].str.extract("(\d\d)")
+    df["Artist End Date_clean"] = df["Artist End Date"].str.extract("(\d\d\d\d)")
+    df["Artist End Date_clean"] = df["Artist End Date"].str.extract("(-?\d\d\d\d\|?){1,10}")
+    return df
